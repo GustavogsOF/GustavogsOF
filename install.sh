0a820f081f2213d54fe7475f0f5cc17e40948468
gustavogsOF <ggama0371@gmail.com>
2025-02-18 08:15:33 -0300

    fix and update file for install desktop

--- install.sh
+++ install.sh
@@ -1,8 +1,29 @@
 # install the packages
 
-apt install git golang python 
+apt install git 
 
-apt install build-essential htop cmatrix fastfetch
+apt install golang 
+
+apt install python 
+
+apt install build-essential 
+
+apt install htop 
+
+apt install cmatrix 
+
+apt install fastfetch
+
+# hello for all
 
 apt install x11-repo
 
+apt install xfce4
+
+apt install firefox
+
+apt install fastfetch
+
+apt install htop
+
+apt install termux-x11-nightly
