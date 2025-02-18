b71a61f490429079503176e3251152471cd1b914
gustavogsOF <ggama0371@gmail.com>
2025-02-17 21:36:20 -0300

    update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
 # install the packages
 
-apt install git golang python build-essential htop cmatrix fastfetch
+apt install git golang python 
+
+apt install build-essential htop cmatrix fastfetch
+
+apt install x11-repo
+
